af3fcbcf975b8c0021e571815fcbf411f4dcb449
michael.jow.williams@gmail.com <michael.jow.williams@gmail.com>
2021-01-04 02:27:55 +0000

    Bugs: Fixes to Image Carousel

--- FAC-Website/app.js
+++ FAC-Website/app.js
@@ -1,4 +1,5 @@
 const icImages = document.querySelector('.ic__images');
+const Images = document.querySelectorAll('.ic__images img');
 const icButtons = document.querySelectorAll('.ic__button');
 const numOfImages = document.querySelectorAll('.ic__images img').length;
 let index = 1;
@@ -19,5 +20,13 @@ icButtons.forEach(button => {
     }
     
     icImages.style.transform = `translateX(${translateX}px)`;
+	
+	Images.forEach((image, NewIndex) =>{
+		if(NewIndex === index - 1){
+			image.classList.add('active');
+		}else{
+			image.classList.remove('active');
+		}
+	});
   });
 });
